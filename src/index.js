a71c4b33948adc381f6be713286e4a32c5420370
Laraoldebolhaar <laraoldebolhaar@gmail.com>
2024-03-18 16:18:09 -0400

    Improved the User Experience

--- src/index.js
+++ src/index.js
@@ -12,12 +12,18 @@ function generateWorkout(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
+  let answer = instructionsInput.value.toLowerCase();
   let apiKey = "30560794ofe15t2924a35bbc6345451b";
   let context =
-    "Act as if you are a personal trainer and expert on high intensity workouts. Do make sure the workouts can be done without equipment and take between 10-30 min. Present the workout in html format. First, list the all the movements and after that give a short instruction of no more than 3 sentenses for the whole workout.";
-  let userPrompt = `Generate a high intensity workout focusing on ${instructionsInput.value}`;
+    "Act as if you are a personal trainer and expert on high intensity workouts. Do make sure the workouts can be done without equipment and take between 10-30 min. Present the workout in html format. First, list the all the movements and after that give a short instruction of no more than 3 sentenses for the whole workout. Finish with an inspirational fitness quote";
+  let userPrompt = `Generate a high intensity workout focusing on ${answer}`;
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${userPrompt}&context=${context}&key=${apiKey}`;
+
+  let workoutElement = document.querySelector("#workout");
+  workoutElement.classList.remove("hidden");
+  workoutElement.innerHTML = `✨ Generating your HIIT workout focusing on ${answer}<div class="loader"><span class="loader__dot">.</span><span class="loader__dot">.</span><span class="loader__dot">.</span></div>
+`;
 
   axios.get(apiUrl).then(displayWorkout);
 }
