d151f04d9580124476dff5a6a78c07b9bbe85135
Laraoldebolhaar <laraoldebolhaar@gmail.com>
2024-03-18 12:57:22 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generateWorkout(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "30560794ofe15t2924a35bbc6345451b";
   let context =
-    "Act as if you are a personal trainer and expert on high intensity workouts. Do make sure the workouts can be done without equipment and take between 10-30 min. Present the workout in a simple list format. First, list the all the movements and after that give a short instruction of no more than 3 sentenses for the whole workout.";
+    "Act as if you are a personal trainer and expert on high intensity workouts. Do make sure the workouts can be done without equipment and take between 10-30 min. Present the workout in html format. First, list the all the movements and after that give a short instruction of no more than 3 sentenses for the whole workout.";
   let userPrompt = `Generate a high intensity workout focusing on ${instructionsInput.value}`;
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${userPrompt}&context=${context}&key=${apiKey}`;
